61de28840170832a2b18f2c3afa95ffb4c06a99a
the-speaker3 <the_speaker3@protonmail.com>
2025-03-11 13:08:55 +0100

    chore: Linting

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -21,18 +21,13 @@ const config = {
   collectCoverage: true,
 
   // An array of glob patterns indicating a set of files for which coverage information should be collected
-  collectCoverageFrom: [
-    './src/**/*.ts'
-  ],
+  collectCoverageFrom: ['./src/**/*.ts'],
 
   // The directory where Jest should output its coverage files
   coverageDirectory: 'coverage',
 
   // An array of regexp pattern strings used to skip coverage collection
-  coveragePathIgnorePatterns: [
-    "/node_modules/",
-    "/src/seed.ts"
-  ],
+  coveragePathIgnorePatterns: ['/node_modules/', '/src/seed.ts'],
 
   // Indicates which provider should be used to instrument code for coverage
   // coverageProvider: "babel",
@@ -163,10 +158,7 @@ const config = {
   // ],
 
   // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
-  testPathIgnorePatterns: [
-    '/node_modules/',
-    '<rootDir>/src/__tests__/routes/',
-  ],
+  testPathIgnorePatterns: ['/node_modules/', '<rootDir>/src/__tests__/routes/'],
 
   // The regexp pattern or array of patterns that Jest uses to detect test files
   // testRegex: [],
