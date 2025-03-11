d08cf6c6bb2bd6c20b6c980b212c7b1857a14f79
the-speaker3 <the_speaker3@protonmail.com>
2025-03-11 17:24:15 +0100

    fix: Dependencies

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -2,14 +2,12 @@ module.exports = {
   root: true,
   extends: [
     'next/core-web-vitals',
-    'tailwindcss',
     'prettier',
     'stylelint-config-standard-scss',
     'stylelint-config-prettier-scss',
   ],
-  plugins: ['@typescript-eslint', 'tailwindcss'],
+  plugins: ['@typescript-eslint'],
   rules: {
-    'tailwindcss/classnames-order': 'off',
     'selector-class-pattern': null,
   },
   parser: '@typescript-eslint/parser',
@@ -24,12 +22,8 @@ module.exports = {
       extends: [
         'next/core-web-vitals',
         'plugin:@typescript-eslint/recommended',
-        'plugin:tailwindcss/recommended',
         'prettier',
       ],
-      rules: {
-        'tailwindcss/classnames-order': 'off',
-      },
     },
   ],
 };
